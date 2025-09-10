976a7cdd9a904df83ee1fcfd4d3317953ae78649
Prajwal Ghurde <131018809+prajwalghurde@users.noreply.github.com>
2025-09-11 02:02:07 +0530

    Update axios.js

--- oralvis-frontend/src/api/axios.js
+++ oralvis-frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://oralvis-flvs.onrender.com" });
+const API = axios.create({ baseURL: import.meta.env.VITE_API_URL });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
