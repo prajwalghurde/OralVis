73a6e06a5182e9360bc2d7a036ff1361744f877f
Prajwal Ghurde <131018809+prajwalghurde@users.noreply.github.com>
2025-09-11 11:24:43 +0530

    Update axios.js

--- oralvis-frontend/src/api/axios.js
+++ oralvis-frontend/src/api/axios.js
@@ -1,13 +1,8 @@
 import axios from "axios";
 
-// Set base URL here (change if needed for production)
-export const API_BASE_URL = "https://oralvis-flvs.onrender.com";
-
 const API = axios.create({
-  baseURL: API_BASE_URL,
-});
+  baseURL: process.env.REACT_APP_API_URL });
 
-// Add auth token automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) req.headers.Authorization = `Bearer ${token}`;
